8c2f9b0e63f4990e6bbe5d1b264cb9e400cb88b5
fly1115 <1156628651@qq.com>
2018-08-18 23:03:01 +0800

    主页完成

--- public/back/js/commont.js
+++ public/back/js/commont.js
@@ -1,7 +1,7 @@
 /**
  * Created by funiannian on 2018/8/18.
  */
-$(function(){
+
     //jquery中ajax的全局事件
     //ajaxSend() 在beforeSend回调函数之后触发
     //ajaxSuccess在success回调函数之后触发
@@ -14,14 +14,63 @@ $(function(){
     })
     //ajaxStop() 在所有ajax请求结束时触发
     //加定时为了模拟进度条效果
-    setInterval(function(){
         $(document).ajaxStop(function(){
             NProgress.done();
         })
-    },500)
+
+    // 用户登录拦截
+    if(location.href.indexOf('login.html') === -1){
+        //  地址栏中没有login.html,说明当前页面不是登录页面,需要登录拦截
+        $.ajax({
+            type:'get',
+            url:'/employee/checkRootLogin',
+            dataType:'json',
+            success:function(info){
+                console.log(info);
+                if(info.success){
+                }
+                if(info.error){
+                    location.href = 'login.html'
+                }
+            }
+        })
+
+    }
 
 
+    $(function(){
+    // 1.分类管理切换功能
+    $('.nav .category').click(function(){
+        $('.nav .child').stop().slideToggle();
+    })
 
+    //2. 左侧侧边栏切换功能
+    $('.icon_menu').click(function(){
+        console.log(1111);
+        // $('.lt_aside').toggleClass('hidemenu');
+        $('.lt_aside').toggleClass("hidemenu");
+        $('.it_main').toggleClass('hidemenu');
+        $('.lt_topbar').toggleClass('hidemenu');
+    })
+    // 3.点击topbar退出按钮, 弹出模态框
+    $('.icon_logout').click(function(){
+        console.log(1111);
+        $('#logoutModal').modal('show');
+    })
+    // 4.点击退出按钮向后台发送ajax
+    $('.btn-logout').click(function(){
+        $.ajax({
+            type:'get',
+            url:'/employee/employeeLogout',
+            dataType:'json',
+            success:function(info){
+                console.log(info);
+                if(info.success){  
+                    location.href = 'login.html';
+                }
+            }
+        })
+    })
 
 
 
